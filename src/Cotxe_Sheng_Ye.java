5ada0bdfaef34df235ca844648544521de61977d
Shengye01 <sye@nigul.cide.es>
2020-12-09 16:43:27 +0100

    update

--- src/Cotxe_Sheng_Ye.java
+++ src/Cotxe_Sheng_Ye.java
@@ -5,13 +5,16 @@
  * Created by: cide
  * Date : 6/12/20
  * Description:
+ * Heu de crear una classe Cotxe que implementi la interface i que sigui una subclasse de la classe abstracta. Aquesta classe s’ha d’anomenar:
+ * Cotxe_nom_llinatge.java
+ * Addicionalment, creeu una classe TestCotxe_nom_llinatge.java que comprovi el correcte funcionament de la classe Cotxe.
  **/
 import java.util.Random;
 public class Cotxe_Sheng_Ye extends CotxeAbstracte implements InterfaceCotxe {
 
     protected EstatsMotorCotxe estadocoche;
 
-    //Constructor
+    //Constructores en que cogemos de los tipos de conceptos
 
 
     public Cotxe_Sheng_Ye(String marca, String model, TipusCanvi tipusCanvi, EstatsMotorCotxe estadocoche) {
@@ -20,7 +23,7 @@ public class Cotxe_Sheng_Ye extends CotxeAbstracte implements InterfaceCotxe {
     }
 
     //Methods
-
+//Arranca el motor y da salida de que esta arrancado como tal
     @Override
     public void arrancarMotor() throws Exception {
         if (estadocoche == EstatsMotorCotxe.Aturat) {
@@ -35,18 +38,18 @@ public class Cotxe_Sheng_Ye extends CotxeAbstracte implements InterfaceCotxe {
     public EstatsMotorCotxe comprovaMotor() {
         return this.estadocoche;
     }
-
+//Recoge las revoluciones que va en el coche
     @Override
     public int getRevolucions() {
         if (estadocoche == EstatsMotorCotxe.Aturat) {
             return  0;
         }
         else{
-            Random numerorandom = new Random();
-            return numerorandom.nextInt(6500+1);
+            Random numeroaleatorio = new Random();
+            return numeroaleatorio.nextInt(6500+1);
         }
     }
-
+//Enseña cuando para el motor
     @Override
     public void aturarMotor() throws Exception {
         if (estadocoche == EstatsMotorCotxe.Aturat){
